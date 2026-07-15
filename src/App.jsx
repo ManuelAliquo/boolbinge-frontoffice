50455976e2adc385b00ecf23e30a831112c5646b
ManuelAliquo <manuelaliquo2103@gmail.com>
2026-07-15 21:15:06 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <>
+      <div className="container my-5 pt-4">
+        <h1 className="fw-bold text-decoration-underline">THE</h1>
+      </div>
+    </>
+  );
+}
